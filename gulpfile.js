77d6023e42f5fe5215a9e50e99450b88391d7ed4
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-03 01:47:03 -0600

    Basic setup from my API starter project. Installed socket.io. Made a few small edits. Updated some of the npm modules that were old.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    babel = require('gulp-babel');
+
+// DEFAULT
+gulp.task('default', ['jshint']);
+
+// JSHINT
+gulp.task('jshint', ['babel'], function() {
+  return gulp.src('./dist/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// BABEL
+gulp.task('babel', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// WATCH
+gulp.task('watch', ['jshint'], function() {
+  gulp.watch('./src/**/*.js', ['jshint']);
+});
